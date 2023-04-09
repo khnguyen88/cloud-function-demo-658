c827f885e65ba48f52638857678da2ebae93f432
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-04-09 19:44:07 -0400

    Added in basic sections, containers, and classname for the App template. Added in the styles.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import CloudFunction from './components/CloudFunction/CloudFunction';
 
 function App() {
   return (
     <div className="App">
-      <CloudFunction/>
+      <header className="app-header">
+        <div>
+          Welcome to the Dad Zone!!! 
+        </div>
+        <div>
+          (❍ᴥ❍ʋ)
+        </div>
+      </header>
+      <main className="app-main"> 
+        <CloudFunction/>
+      </main>       
+      <footer className="app-footer">
+          &#169; Big Poppa Inc., 2023.
+      </footer>
     </div>
   );
 }
